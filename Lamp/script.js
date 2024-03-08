0924e330f8ce0ef3bb0d5a6182ee3ef90280dc7d
Vinicius Silveira <vsribeiro87@gmail.com>
2024-03-08 18:05:06 -0300

    updateLampv1.3

--- Lamp/script.js
+++ Lamp/script.js
@@ -4,26 +4,25 @@ var btnoff = document.getElementById("off");
 var btchange = document.getElementById("changeLamp");
 
 function on() {
-    lamp.src = ("./images/ligada.jpg");
+    lamp.src = "./images/ligada.jpg";
 }
 
 function off() {
-    lamp.src = ("./images/desligada.jpg");
+    lamp.src = "./images/desligada.jpg";
 }
 
-lamp.addEventListener(
-    'click',
-    () => {
-        lamp.src = ("./images/quebrada.jpg");
-        btnon.removeAttribute('onclick');
-        btnoff.removeAttribute('onclick');
-    })
+function broke() {
+    lamp.src = "./images/quebrada.jpg";
+    btnon.removeAttribute('onclick');
+    btnoff.removeAttribute('onclick');
+}
 
-btchange.addEventListener(
-    'click',
-    () => {
-        lamp.src = ("./images/desligada.jpg");
-        btnon.setAttribute("onclick", "on()");
-        btnoff.setAttribute("onclick", "off()");
-    }
-)
+function replace() {
+    lamp.src = "./images/desligada.jpg";
+    btnon.setAttribute("onclick", "on()");
+    btnoff.setAttribute("onclick", "off()");
+}
+
+lamp.addEventListener('click', broke);
+
+btchange.addEventListener('click', replace);
