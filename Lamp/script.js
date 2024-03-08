55e58fa6b060ad220de451deb8cb887e9cc8da67
Vinicius Silveira <vsribeiro87@gmail.com>
2024-03-08 13:06:37 -0300

    update8mar

--- Lamp/script.js
+++ Lamp/script.js
@@ -1,1 +1,21 @@
-  
+let boxlamp = document.getElementById("lamp");
+let lamp = document.getElementById("lampimg");
+let btnon = document.getElementById("on");
+let btnoff = document.getElementById("off");
+
+function on() {
+    lamp.src = ("./images/ligada.jpg");
+}
+
+function off() {
+    lamp.src = ("./images/desligada.jpg");
+}
+
+lamp.addEventListener('click',
+    () => {
+        lamp.src = ("./images/quebrada.jpg");
+        btnon.onclick = ("");
+        btnoff.onclick = ("");
+        boxlamp.onmouseover = ("");
+        boxlamp.onmouseout = ("");
+    })
