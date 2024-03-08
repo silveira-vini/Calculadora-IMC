98197a4f5d6efb294ca4228ad4eecca9ba9fe7f1
Vinicius Silveira <vsribeiro87@gmail.com>
2024-03-08 13:59:20 -0300

    update8marv1

--- Lamp/script.js
+++ Lamp/script.js
@@ -1,21 +1,60 @@
-let boxlamp = document.getElementById("lamp");
-let lamp = document.getElementById("lampimg");
-let btnon = document.getElementById("on");
-let btnoff = document.getElementById("off");
+var boxlamp = document.getElementById("lamp");
+var lamp = document.getElementById("lampimg");
+var btnon = document.getElementById("on");
+var btnoff = document.getElementById("off");
+var btchange = document.getElementById("changeLamp");
+let broke = false;
 
-function on() {
-    lamp.src = ("./images/ligada.jpg");
+console.log(broke);
+
+
+if (broke == false) {
+
+    btnon.addEventListener(
+        'click',
+        () => {
+            lamp.src = ("./images/ligada.jpg");
+        }
+    )
+
+    btnoff.addEventListener(
+        'click',
+        () => {
+            lamp.src = ("./images/desligada.jpg");
+        }
+    )
 }
 
-function off() {
-    lamp.src = ("./images/desligada.jpg");
+else {
+    btnon.addEventListener(
+        'click',
+        () => {
+            lamp.src = ("./images/quebrada.jpg");
+        }
+    )
+
+    btnoff.addEventListener(
+        'click',
+        () => {
+            lamp.src = ("./images/quebrada.jpg");
+        }
+    )
+
 }
 
-lamp.addEventListener('click',
+lamp.addEventListener(
+    'click',
     () => {
         lamp.src = ("./images/quebrada.jpg");
-        btnon.onclick = ("");
-        btnoff.onclick = ("");
-        boxlamp.onmouseover = ("");
-        boxlamp.onmouseout = ("");
+        broke = true;
+        console.log(broke);
     })
+
+btchange.addEventListener(
+    'click',
+    () => {
+        lamp.src = ("./images/desligada.jpg");
+        broke = false;
+    }
+
+)
