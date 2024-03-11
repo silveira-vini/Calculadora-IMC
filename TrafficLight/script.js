dfa5e1a570257a528ed3bbdb5dc2c6180878ea05
Vinicius Silveira <vsribeiro87@gmail.com>
2024-03-11 16:13:11 -0300

    update11mar-v1.1

--- TrafficLight/script.js
+++ TrafficLight/script.js
@@ -5,7 +5,7 @@ let yellow = document.getElementById("y");
 let green = document.getElementById("g");
 let automatic = document.getElementById("a");
 
-let images = ["./images/vermelho.png", "./images/amarelo.png", "./images/verde.png"]
+const images = ["./images/vermelho.png", "./images/amarelo.png", "./images/verde.png"]
 let indiceAtual = 0;
 
 
@@ -22,13 +22,16 @@ function turnGreen() {
     image.src = "./images/verde.png";
 }
 
+
 function changeImg() {
+
     image.setAttribute("src", images[indiceAtual]);
-    indiceAtual = ((indiceAtual + 1) % images.length);
-    setInterval(changeImg, 1000);
+    indiceAtual = (indiceAtual + 1) % 3;
+    
 }
+
 
 red.addEventListener("click", turnRed);
 yellow.addEventListener("click", turnYellow);
 green.addEventListener("click", turnGreen);
-automatic.addEventListener("click", changeImg);
+automatic.addEventListener("click", interval);
