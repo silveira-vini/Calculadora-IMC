815708293a15b6db3ce416f45e972e4af14860b1
Vinicius Silveira <vsribeiro87@gmail.com>
2024-03-15 15:42:50 -0300

    upte15marFinal

--- TrafficLight/script.js
+++ TrafficLight/script.js
@@ -1,26 +1,31 @@
 const img = document.getElementById('img');
 const buttons = document.getElementById('buttons');
+let colorIndex = 0;
+let intervalId = null;
 
 const trafficLight = (event) => {
-
-    if (event.target.id == 'red') {
-        turnOn['red']();
-    }
-
-    if (event.target.id == 'yellow') {
-        turnOn['yellow']();
-    }
-
-    if (event.target.id == 'green') {
-        turnOn['green']();
-    }
-
+    stopAutomatic();
+    turnOn[event.target.id]();
 };
+
+
+const changeColor = () => {
+    const colors = ['red', 'yellow', 'green'];
+    const color = colors[colorIndex];
+    turnOn[color]();
+    colorIndex = ++colorIndex % 3;
+
+}
+
+const stopAutomatic = () => {
+    clearInterval(intervalId);
+}
 
 const turnOn = {
     'red': () => img.src = './images/vermelho.png',
     'yellow': () => img.src = './images/amarelo.png',
-    'green': () => img.src = './images/verde.png'
+    'green': () => img.src = './images/verde.png',
+    'automatic': () => intervalId = setInterval(changeColor, 1000)
 }
 
 buttons.addEventListener('click', trafficLight);
